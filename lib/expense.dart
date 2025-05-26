807bf3744a745b7ecbeaf7209c2592b54aaf0fb1
PurvaVyawahare <purvavyawahare17@gmail.com>
2025-05-26 15:58:18 +0530

    Created the basic display

---
+++ lib/expense.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Expense extends StatefulWidget {
+  const Expense({super.key});
+  @override
+  State<Expense> createState() {
+    return _ExpenseState();
+  }
+}
+
+class _ExpenseState extends State<Expense> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('This is our chart'),
+          SizedBox(height: 20),
+          Text('This is our list'),
+        ],
+      ),
+    );
+  }
+}
